b3f973464534a3421c245fd225caffd2df8d9f0d
philipkariuki <kariukiphil@gmail.com>
2019-07-22 14:04:16 +0300

    registers classes in appmatic/admin.py

--- appmatic/admin.py
+++ appmatic/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Location,Image,tags, Category
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+
+admin.site.register(Location)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Category)
+admin.site.register(tags)
