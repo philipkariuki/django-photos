39252a9787317e1c0dd911c0674dd0a7d8d00648
philipkariuki <kariukiphil@gmail.com>
2019-07-22 04:48:09 +0300

    adds the app appmatic to projectphil settings.py

--- projectphil/settings.py
+++ projectphil/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appmatic.apps.AppmaticConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
