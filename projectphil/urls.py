64d234f3ead04b17db2f881d6a84983bff8a7b17
philipkariuki <kariukiphil@gmail.com>
2019-07-22 14:10:58 +0300

    updates projectphil/urls.py to redirect to main page without typing /appmatic in browser url

--- projectphil/urls.py
+++ projectphil/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^appmatic/',include('appmatic.urls'))
+    url(r'',include('appmatic.urls'))
 ]
